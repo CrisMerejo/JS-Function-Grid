11efc38da91d8b84f270d22d0c106fb29cf3cb07
Cris <crcastillo@cristoreyny.org>
2017-02-03 14:53:37 +0000

    updated scripts

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //Write your function calls below this line
+setColor(2,3,"black");
+setColor(2,6,"black");
+setColor(6,4,"black");
+setColor(6,5,"black");
+setColor(6,3,"black");
+setColor(6,6,"black");
